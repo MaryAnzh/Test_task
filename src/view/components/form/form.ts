81b5c020add224f38553b53c5bc4a2fe75c2367d
MaryAnzh <maryang@mail.ru>
2022-11-12 18:29:23 +0300

    frat: add button style

--- src/view/components/form/form.ts
+++ src/view/components/form/form.ts
@@ -5,17 +5,31 @@ type Atr = {
 
 export class FormComponent {
     form = this.createElement('form', 'form', null);
+    formElemtWrap = this.createElement('div', 'form__elem-wrap', null);
+    input = this.createElement('input', 'form__elem-wrap__field', [{ name: 'type', value: 'text' }, { name: 'required', value: 'true' }, { name: 'placeholder', value: 'Выш номер,,,' }]);
+    submitButton = this.createElement('button', 'form__elem-wrap__button', [{ name: 'type', value: 'submit' }]);
 
     constructor() {
-        this.form.textContent = 'form';
+        this.createForm();
     }
 
-    createElement(name: string, className: string, atr: Atr[] | null): Node {
+    createElement(name: string, className: string, atr: Atr[] | null): HTMLElement {
         const elem = document.createElement(name);
         elem.classList.add(className);
         if (atr !== null) {
             atr.forEach(atr => elem.setAttribute(atr.name, atr.value));
         }
         return elem;
+    }
+
+    createForm(): void {
+        this.submitButton.textContent = 'Заказать';
+        const test = this.createElement('div', 'test', null);
+        this.formElemtWrap.append(this.input, this.submitButton);
+        this.form.append(this.formElemtWrap, test);
+    }
+
+    submit(e: Event): void {
+        e.preventDefault();
     }
 }
